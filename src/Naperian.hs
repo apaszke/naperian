759195d435fed1e9cbb8671185847278bd04aa14
Adam Paszke <adam.paszke@gmail.com>
2019-06-03 21:54:17 +0200

    Relax Native constraints, implement Functor (Dim n)

--- src/Naperian.hs
+++ src/Naperian.hs
@@ -2,17 +2,17 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 
 module Naperian where
 
+import Prelude hiding (lookup)
+
 import GHC.TypeLits
 import Data.Proxy (Proxy(..))
-import Data.Kind (Type)
+import Data.Kind (Type, Constraint)
 
 --------------------------------------------------------------------------------
 -- Dummy definitions simulating a dynamic tensor type exposed using FFI
@@ -39,6 +39,9 @@ itemInt :: DynamicTensor -> Int
 itemInt (Scalar x) = x
 itemInt _          = error "itemInt called on a non-scalar tensor!"
 
+dynamicTranspose :: DynamicTensor -> DynamicTensor
+dynamicTranspose = error "missing dynamicTranspose"
+
 --------------------------------------------------------------------------------
 -- Functorial tensors
 --------------------------------------------------------------------------------
@@ -51,124 +54,41 @@ itemInt _          = error "itemInt called on a non-scalar tensor!"
 -- family that represents dependently typed multidimensional arrays that attempt
 -- to use the optimized C++ representation for all types that support it.
 
+class IsTensor a where
+    dunfold :: DynamicTensor -> [a]
+    dfold :: [a] -> DynamicTensor
+
+instance IsTensor Int where
+    dunfold t = map itemInt $ iterateOuter t
+    dfold l = tensorConcat $ map Scalar l
+
+instance (IsTensor a) => IsTensor (Dim n a) where
+    dunfold t = map Tensor $ iterateOuter t
+    dfold l = tensorConcat $ map (\(Tensor t) -> t) l
+
 data Dim (n :: Nat) a where
-    Tensor :: (IsTensor a ~ True) => DynamicTensor -> Dim n a
+    Tensor :: (IsTensor a) => DynamicTensor -> Dim n a
     -- [a] should technically be Vector n a, but I want to play around with
     -- other things first. Fixing this is a TODO.
-    Native :: (IsTensor a ~ False) => [a] -> Dim n a
-
-type family IsTensor (a :: Type) :: Bool where
-    IsTensor Int = True
-    IsTensor (Dim _ a) = IsTensor a
-    IsTensor _ = False
+    Native :: [a] -> Dim n a
 
 instance Show a => Show (Dim n a) where
     show (Native l) = "Native " ++ show l
     show (Tensor l) = "Tensor " ++ show l
 
 --------------------------------------------------------------------------------
--- Helpers
+-- Functor instance
 --------------------------------------------------------------------------------
 
--- TFoldable is a bijection between DynamicTensor and more strict types
--- obtained by unfolding a dimension of a DynamicTensor into a Haskell world, or
--- the other way.
---
--- XXX: dunfold is partial, because we cannot do bounds checking on the type level!
-class TFoldable a where
-    dunfold :: DynamicTensor -> [a]
-    dfold :: [a] -> DynamicTensor
-
-instance TFoldable Int where
-    dunfold t = map itemInt $ iterateOuter t
-    dfold l = tensorConcat $ map Scalar l
-
-instance (IsTensor a ~ True) => TFoldable (Dim n a) where
-    dunfold t = map Tensor $ iterateOuter t
-    dfold l = tensorConcat $ map (\(Tensor t) -> t) l
-
-
-
-data SingIsTensor a where
-    SIsTensor   :: (IsTensor a ~ True, TFoldable a) => SingIsTensor a
-    SNotATensor :: (IsTensor a ~ False)             => SingIsTensor a
-
-
-class SingIsTensorI' a (b :: Bool) where
-    singIsTensor' :: Proxy b -> SingIsTensor a
-
-instance (TFoldable a, IsTensor a ~ True) => SingIsTensorI' a True where
-    singIsTensor' _ = SIsTensor
-
-instance (IsTensor a ~ False) => SingIsTensorI' a False where
-    singIsTensor' _ = SNotATensor
-
-class SingIsTensorI a where
-    singIsTensor :: SingIsTensor a
-
-instance (IsTensor a ~ b, SingIsTensorI' a b) => SingIsTensorI a where
-    singIsTensor = singIsTensor' (Proxy :: Proxy b)
-
-fmap'' :: SingIsTensor a -> SingIsTensor b -> (a -> b) -> Dim n a -> Dim n b
-fmap'' (SIsTensor)   (SIsTensor)   f (Tensor t) = Tensor $ dfold    $ fmap f    $ dunfold t
-fmap'' (SIsTensor)   (SNotATensor) f (Tensor t) = Native $ fmap f   $ dunfold t
-fmap'' (SNotATensor) (SIsTensor)   f (Native l) = Tensor $ dfold    $ fmap f l
-fmap'' (SNotATensor) (SNotATensor) f (Native l) = Native $ fmap f l
-
-
-fmap_ :: (SingIsTensorI a, SingIsTensorI b) => (a -> b) -> Dim n a -> Dim n b
---fmap_ :: (a -> b) -> Dim n a -> Dim n b
-fmap_ f x = fmap'' singIsTensor singIsTensor f x
+instance Functor (Dim n) where
+    fmap f (Tensor t) = Native $ fmap f   $ dunfold t
+    fmap f (Native l) = Native $ fmap f l
 
 --------------------------------------------------------------------------------
--- Dependent Functor
+-- Naperian functor
 --------------------------------------------------------------------------------
 
--- Unfortunately, while one can easily check that (Dim n) is a functor for
--- every n, it is very hard to convince the Haskell typechecker that it is the case.
--- Hence, instead of implementing an instance of (Functor (Dim n)) we define a new
--- type class Functor' f a b, which allows us to adjust the implementation of fmap
--- depending on the types of a and b.
-class Functor' f a b where
-    fmap' :: (a -> b) -> f a -> f b
-
--- The following simply enumerates all cases of a and b being a tensor or not, but we have
--- to use the extra level of indirection through DimFunctor' to avoid overlapping instances
--- errors. One can easily convince themself that they _are not_ overlapping, but GHC doesn't
--- care about constraints when picking the class instances and hence we need to resort to those
--- ugly tricks. More about this technique can be found at
--- https://kseo.github.io/posts/2017-02-05-avoid-overlapping-instances-with-closed-type-families.html
-instance (IsTensor a ~ at, IsTensor b ~ bt, DimFunctor' f a b at bt) => Functor' f a b where
-    fmap' = dimfmap' (Proxy :: Proxy at) (Proxy :: Proxy bt)
-
-class DimFunctor' f a b (at :: Bool) (bt :: Bool) where
-    dimfmap' :: Proxy at -> Proxy bt -> (a -> b) -> f a -> f b
-
-instance (IsTensor a ~ False, IsTensor b ~ False) => DimFunctor' (Dim n) a b 'False 'False where
-    dimfmap' _ _ f (Native l) = Native $ fmap f l
-
-instance (TFoldable a, IsTensor a ~ True, IsTensor b ~ False) => DimFunctor' (Dim n) a b 'True 'False where
-    dimfmap' _ _ f (Tensor t) = Native $ fmap f $ dunfold t
-
-instance (IsTensor a ~ False, IsTensor b ~ True, TFoldable b) => DimFunctor' (Dim n) a b 'False 'True where
-    dimfmap' _ _ f (Native l) = Tensor $ dfold $ fmap f l
-
-instance (IsTensor a ~ True, IsTensor b ~ True, TFoldable a, TFoldable b) => DimFunctor' (Dim n) a b 'True 'True where
-    dimfmap' _ _ f (Tensor t) = Tensor $ dfold $ fmap f $ dunfold t
-
---------------------------------------------------------------------------------
--- The Naperian property
---------------------------------------------------------------------------------
-
--- NB: While it really is the case that we have a Functor' (Dim n) instance for
--- all types a and b, Haskell does not accept my (forall a b. Functor' (Dim n) a b)
--- constraint :(
--- For the same reason I cannot reproduce the `Functor f => Naperian f` part of the
--- paper...
---instance {-# OVERLAPPABLE #-} (forall a b. Functor' f a b) => Functor f where
-    --fmap = fmap'
-
-class Naperian f where
+class Functor f => Naperian f where
     type Index f :: Type
     lookup :: f a -> Index f -> a
     tabulate :: (Index f -> a) -> f a
@@ -176,31 +96,43 @@ class Naperian f where
 
 instance Naperian (Dim n) where
     type Index (Dim n) = Int -- TODO: Use finite naturals
-    lookup = undefined
-    tabulate = undefined
-    positions = undefined
+    lookup = error "missing lookup"
+    tabulate = error "missing tabulate"
+    positions = error "missing positions"
+
+--------------------------------------------------------------------------------
+-- Operator implementations
+--------------------------------------------------------------------------------
+
+transpose :: (IsTensor a => IsTensor a) => Dim n (Dim m a) -> Dim m (Dim n a)
+transpose x@(Native _) = tabulate . fmap tabulate . flip . fmap lookup . lookup $ x
+transpose   (Tensor t) = Tensor $ dynamicTranspose t
 
 --------------------------------------------------------------------------------
 -- Some variables if you want to play around in ghci
 --------------------------------------------------------------------------------
+
 x :: Dim 2 (Dim 2 Int)
 x = Tensor $ TwoDim [[1, 2], [3, 4]]
 
 y :: Dim 2 Int
 y = Tensor $ OneDim [1, 2]
 
+-- Unfortunately fmap is heavily Native-biased
 yp2 :: Dim 2 Int
-yp2 = fmap' (+2) y
+yp2 = fmap (+2) y
 -- *Naperian> yp2
--- Tensor OneDim [3, 4]
+-- Native [3, 4]
 
 xp2 :: Dim 2 (Dim 2 Int)
-xp2 = fmap' (fmap' (+2)) x
+xp2 = fmap (fmap (+2)) x
 -- *Naperian> xp2
--- Tensor TwoDim [[3, 4], [5, 6]]
+-- Native [Native [3, 4], Native [5, 6]]
 
 xshow :: Dim 2 String
-xshow = fmap' show x
--- NB: Notice how the type seamlessly changes to Native once we get a list of strings!
+xshow = fmap show x
 -- *Naperian> xshow
 -- Native ["Tensor OneDim [1,2]","Tensor OneDim [3,4]"]
+
+z_dynamic = transpose x
+z_naperian = transpose $ fmap (fmap show) x
